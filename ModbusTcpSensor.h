4e4c11181de7562dfe9a056c05afa1591b8c0797
OABsoftware <39148588+OABsoftware@users.noreply.github.com>
2025-07-23 15:07:17 +0200

    Update ModbusTcpSensor.h
    Enhanced timeout handling

--- ModbusTcpSensor.h
+++ ModbusTcpSensor.h
@@ -15,19 +15,22 @@ class ModbusClient
 
 
   private:
-    bool mbWaitingForResponse = false;
     byte mbExpectedRegisterCount = 0;
     int miExpectedResponseLength = 0;
     int miReceivedResponseLength = 0;
+    unsigned long mlTimeout = 0;
 
 
   public:
     const int MODBUS_SUCCESS = 0;
     const int MODBUS_IDLE = -1;
-    const int MODBUS_CONNECT_ERROR = -10;
-    const int MODBUS_WAITING_FOR_RESPONSE = -11;
-    const int MODBUS_INVALID_RESPONSE = -12;
-    const int MODBUS_INVALID_FNC = -13;
+    const int MODBUS_CONNECT_ERROR = -2;
+    const int MODBUS_TIMEOUT = -3;
+    const int MODBUS_WAITING_FOR_RESPONSE = -4;
+    const int MODBUS_INVALID_RESPONSE = -5;
+    const int MODBUS_INVALID_FNC = -6;
+
+    bool IsWaitingForResponse = false;
 
 
     ModbusClient()
@@ -44,68 +47,73 @@ class ModbusClient
 
     void beginRequest(WiFiClient& oWifiClient, byte bUnitID, uint16_t uRegistersStartAddress, byte bRegisterCount) 
     {
-      if(! mbWaitingForResponse)
-      {
-        oWifiClient.clear();
+      oWifiClient.clear();
 
-        byte abRequest[] = {0, 1, 0, 0, 0, 6, bUnitID, FNC_READ_REGS, (byte)((uRegistersStartAddress >> 8) & 0xFF), (byte)(uRegistersStartAddress & 0xFF), 0, bRegisterCount};
-        oWifiClient.write(abRequest, sizeof(abRequest));
+      byte abRequest[] = {0, 1, 0, 0, 0, 6, bUnitID, FNC_READ_REGS, (byte)((uRegistersStartAddress >> 8) & 0xFF), (byte)(uRegistersStartAddress & 0xFF), 0, bRegisterCount};
+      oWifiClient.write(abRequest, sizeof(abRequest));
 
-        mbExpectedRegisterCount = bRegisterCount;
-        miExpectedResponseLength = DATA_IX + bRegisterCount * 2;
-        miReceivedResponseLength = 0;
+      mbExpectedRegisterCount = bRegisterCount;
+      miExpectedResponseLength = DATA_IX + bRegisterCount * 2;
+      miReceivedResponseLength = 0;
 
-        mbWaitingForResponse = true;
-      }
+      IsWaitingForResponse = true;
+      mlTimeout = millis() + 2000;
     }
 
 
     int endRequest(WiFiClient& oWifiClient, short *aiRegisterData)
     {
-      if(mbWaitingForResponse)
+      if(IsWaitingForResponse)
       {
         int iRet = MODBUS_WAITING_FOR_RESPONSE;
 
-        if(oWifiClient.available() >= miExpectedResponseLength)
+        if(millis() < mlTimeout)
         {
-          byte abResponseBytes[miExpectedResponseLength];
-
-          int iBytesRead = oWifiClient.readBytes(abResponseBytes, miExpectedResponseLength);
-          if (iBytesRead < miExpectedResponseLength) 
+          if(oWifiClient.available() >= miExpectedResponseLength)
           {
-            oWifiClient.stop();
+            byte abResponseBytes[miExpectedResponseLength];
 
-            ESP_LOGD("modbus_tcp", "Invalid response: %d", iBytesRead);
-            iRet = MODBUS_INVALID_RESPONSE;
-          }
-          else
-          {
-            switch (abResponseBytes[CODE_IX]) 
+            int iBytesRead = oWifiClient.readBytes(abResponseBytes, miExpectedResponseLength);
+            if (iBytesRead < miExpectedResponseLength) 
             {
-              case FNC_READ_REGS:
-                for (int i = 0, j = 0; i < mbExpectedRegisterCount; i++, j += 2) 
-                {
-                  aiRegisterData[i] = (int)(abResponseBytes[DATA_IX + j] << 8 | abResponseBytes[DATA_IX + j + 1]);
-                }
-
-                iRet = MODBUS_SUCCESS;
-                break;
-
-              case (FNC_ERR_FLAG | FNC_READ_REGS):
-                ESP_LOGD("modbus_tcp", "Error response: %d", abResponseBytes[ERR_CODE_IX]);
-                iRet = abResponseBytes[ERR_CODE_IX]; // 0x01, 0x02, 0x03 or 0x11
-                break;
-
-              default:
-                ESP_LOGD("modbus_tcp", "Invalid function code: %d", abResponseBytes[CODE_IX]);
-                iRet = MODBUS_INVALID_FNC;
-                break;
+              ESP_LOGD("modbus_tcp", "Invalid response: %d", iBytesRead);
+              iRet = MODBUS_INVALID_RESPONSE;
             }
-          }
+            else
+            {
+              switch (abResponseBytes[CODE_IX]) 
+              {
+                case FNC_READ_REGS:
+                  for (int i = 0, j = 0; i < mbExpectedRegisterCount; i++, j += 2) 
+                  {
+                    aiRegisterData[i] = (int)(abResponseBytes[DATA_IX + j] << 8 | abResponseBytes[DATA_IX + j + 1]);
+                  }
 
-          mbWaitingForResponse = false;
+                  iRet = MODBUS_SUCCESS;
+                  break;
+
+                case (FNC_ERR_FLAG | FNC_READ_REGS):
+                  ESP_LOGD("modbus_tcp", "Error response: %d", abResponseBytes[ERR_CODE_IX]);
+                  iRet = abResponseBytes[ERR_CODE_IX]; // 0x01, 0x02, 0x03 or 0x11
+                  break;
+
+                default:
+                  ESP_LOGD("modbus_tcp", "Invalid function code: %d", abResponseBytes[CODE_IX]);
+                  iRet = MODBUS_INVALID_FNC;
+                  break;
+              }
+            }
+
+            IsWaitingForResponse = false;
+          }
         }
-        
+        else
+        {
+          iRet = MODBUS_TIMEOUT;
+
+          IsWaitingForResponse = false;
+        }
+
         return iRet;
       }
       
@@ -171,19 +179,22 @@ public:
 
   void update() override 
   {
-    if(! moWifiClient.connected())
+    if(! moModbusClient.IsWaitingForResponse)
     {
-      if (! moWifiClient.connect(host_.c_str(), port_)) 
+      if(! moWifiClient.connected())
       {
-        ESP_LOGE("modbus_tcp", "Failed to connect to the modbus server %s:%d", host_.c_str(), port_);
+        if (! moWifiClient.connect(host_.c_str(), port_)) 
+        {
+          ESP_LOGE("modbus_tcp", "Failed to connect to the modbus server %s:%d", host_.c_str(), port_);
 
-        return;
+          return;
+        }
       }
-    }
+      
+      moWifiClient.setTimeout(1000);
 
-    moWifiClient.setTimeout(1000);
-    
-    moModbusClient.beginRequest(moWifiClient, 1, register_address_, 16);
+      moModbusClient.beginRequest(moWifiClient, 1, register_address_, 16);
+    }
   }
 
 
@@ -191,7 +202,7 @@ public:
   {
     short aiRegisterData[16];
     int iRet = moModbusClient.endRequest(moWifiClient, aiRegisterData);
-    if(iRet == 0)
+    if(iRet == moModbusClient.MODBUS_SUCCESS)
     {
       short iVoltage = aiRegisterData[0];
       short iVoltageScaleFactor = aiRegisterData[8];
